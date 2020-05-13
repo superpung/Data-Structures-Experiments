4e05dd4161d762c58536dafde642aac6e168061f
SuperPung <2283609705@qq.com>
2020-05-13 23:21:18 +0800

    first commit

--- #1/cmake_test/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "test.h"
-#include <stdio.h>
-
-void testPrint(char *printStr)
-{
-    printf("This is a test %s\n", printStr);
-}
--- #1/cmake_test/testAll.cpp
+++
@@ -1,9 +0,0 @@
-#include "test.h"
-#include <stdio.h>
-
-int main()
-{
-    testPrint("Hello, world!\n");
-
-    return 0;
-}
